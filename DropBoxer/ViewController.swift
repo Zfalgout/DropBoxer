87e31cdf521183413e4f55ad5f9f8601542dd6db
Zack Falgout <zfalgout@gmail.com>
2018-08-14 20:04:38 -0500

    Dropbox integration complete.  The app looks for the native Dropbox app to authenticate.  If not there the user is pushed to a sarai web view.

--- DropBoxer/ViewController.swift
+++ DropBoxer/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import SwiftyDropbox
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func connectToDB(_ sender: Any) {
+    
+        DropboxClientsManager.authorizeFromController(UIApplication.shared,
+                                                      controller: self,
+                                                      openURL: { (url: URL) -> Void in
+                                                        UIApplication.shared.openURL(url)
+        })
+    
     }
-
-
+    
 }
 
