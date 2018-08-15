b43547d817f82587104753d7c7ee64b637f048ce
Zack Falgout <zfalgout@gmail.com>
2018-08-14 20:31:46 -0500

    Cutting a new branch for photo library integration.

---
+++ DropBoxer/Supporting_Files/AppDelegate.swift
@@ -0,0 +1,34 @@
+import UIKit
+import SwiftyDropbox
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+       
+        //Initialize a DropboxClient instance
+        DropboxClientsManager.setupWithAppKey("ybr6sblgt4jysvh")
+        
+        return true
+    }
+
+    //The following code handles the redirect back into the SDK from Dropbox.
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if let authResult = DropboxClientsManager.handleRedirectURL(url) {
+            switch authResult {
+            case .success:
+                print("Success! User is logged into Dropbox.")
+            case .cancel:
+                print("Authorization flow was manually canceled by user!")
+            case .error(_, let description):
+                print("Error: \(description)")
+            }
+        }
+        return true
+    }
+
+}
+
